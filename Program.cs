f3589106d95fb6bac5dc55606afcb0fed1d0a3ed
MichaelJoanSI <michaeljr269@gmail.com>
2022-12-19 19:49:42 +0700

    BlazorAppWasm-06

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorAppWasm;
+using MudBlazor.Services;
+using System.Globalization;
+using Microsoft.JSInterop;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+builder.Services.AddLocalization();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
